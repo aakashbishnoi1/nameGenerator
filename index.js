1be73948c8498c4cfec4ad1828b946437ec83a1c
aakashbishnoi1 <8xaakash@gmail.com>
2023-09-14 15:29:12 +0530

    Add the Javascript to add functionality
    added the javascript code to
    -make an api call
    -put the names in an array
    - show the user the names one by one

--- index.js
+++ index.js
@@ -1,3 +1,52 @@
-console.log("Hello world");
+let arr = [];
+async function getNames() {
+    let key = "YAVQjWNSswm0PBJOYQjhVw==oe6cAaYa5XEC32hy"
+    const req = await fetch(`https://api.api-ninjas.com/v1/babynames?gender=neutral`, {
+        headers: { 'X-Api-Key': key}});
+    const res = await req.json();
+    res.map(n => arr.push(n));
+}
+let count = 0;
+const generate = document.getElementById("generate");
+const names = document.getElementById("name"); 
+const prev = document.getElementById("previous");
+const next = document.getElementById("next");
+const btn = document.getElementById("btn");
+generate.addEventListener("click", () => {
+    arr = [];
+    count = 0;
+    names.innerText = "";
+    getNames();
+});
 
+btn.addEventListener("click", () => {
+    if ( arr[0] == undefined) {
+        alert("Please click on generate first");
+    }
+    else {
+    names.innerText = arr[count];
+    }
+    })
+    
+    
+prev.addEventListener("click", () => {
+    if (names.innerText === arr[0]){
+        names.innerText = arr[0] 
+    }
+    else {
+        names.innerText = arr[count - 1];
+        count = count - 1;
+    }
+})
+
+next.addEventListener("click", () => {
+    if (names.innerText === arr[9]){
+        names.innerText = arr[9] 
+    }
+    else {
+        names.innerText = arr[count + 1];
+        count = count + 1;
+    }
+})
+    
 
